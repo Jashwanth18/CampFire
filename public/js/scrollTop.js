c3cd31b70af868a4e01dff364a780e0cacb7e99b
Jashwanth Kurma <jashukurma@gmail.com>
2023-04-06 22:37:17 +0530

    added scrollToTop button

---
+++ public/js/scrollTop.js
@@ -0,0 +1,26 @@
+const scrollTop = function () {
+  // create HTML button element
+  const scrollBtn = document.createElement("button");
+  scrollBtn.innerHTML = "&uarr;";
+  scrollBtn.classList.add("noShow");
+  scrollBtn.setAttribute("id", "scroll-btn");
+  document.body.appendChild(scrollBtn);
+
+  // hide/show button based on scroll distance
+  const scrollBtnDisplay = function () {
+    window.scrollY > window.innerHeight
+      ? scrollBtn.classList.add("show")
+      : scrollBtn.classList.remove("show");
+  };
+  window.addEventListener("scroll", scrollBtnDisplay);
+
+  // scroll to top when button is clicked
+  const scrollWindow = function () {
+    if (window.scrollY != 0) {
+      window.scrollTo(0, 0);
+      scrollWindow();
+    }
+  };
+  scrollBtn.addEventListener("click", scrollWindow);
+};
+scrollTop();
