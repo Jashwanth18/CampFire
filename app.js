7ba5b30f7ec9c0661625dc2ac7340847a23abf95
Jashwanth Kurma <jashukurma@gmail.com>
2023-04-07 12:52:26 +0530

    deploy

--- app.js
+++ app.js
@@ -115,6 +115,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000");
+app.listen(port, () => {
+  console.log(`LISTENING ON PORT ${port}`);
 });
